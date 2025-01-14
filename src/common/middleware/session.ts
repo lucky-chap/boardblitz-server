b8e158a2a3f9da1962e74d5dfca564fc85a35de4
Huncho <hunchodotdev@gmail.com>
2025-01-14 23:45:30 +0000

    make httpOnly false

--- src/common/middleware/session.ts
+++ src/common/middleware/session.ts
@@ -1,20 +1,8 @@
 import type { User } from "@/common/types";
 import PGSimple from "connect-pg-simple";
-import { RedisStore } from "connect-redis";
 import type { Session } from "express-session";
 import session from "express-session";
 import { nanoid } from "nanoid";
-import { createClient } from "redis";
-
-// init redis client
-const redisClient = createClient();
-redisClient.connect().catch(console.error);
-
-// init redis store
-const redisStore = new RedisStore({
-  client: redisClient,
-  prefix: "boardblitz:",
-});
 
 import { pool } from "@/db";
 import { env } from "../utils/envConfig";
@@ -36,15 +24,14 @@ declare module "http" {
   }
 }
 const sessionMiddleware = session({
-  // store: new PGSession({
-  //   pool: pool,
-  //   createTableIfMissing: true,
-  //   errorLog: console.error,
-  //   tableName: "session",
-  // }),
-  store: redisStore,
+  store: new PGSession({
+    pool: pool,
+    createTableIfMissing: true,
+    errorLog: console.error,
+    tableName: "session",
+  }),
   secret: env.SESSION_SECRET || "cat on my keyboard",
-  resave: true,
+  resave: false,
   saveUninitialized: false,
   name: "boardblitz",
   proxy: true,
@@ -52,7 +39,7 @@ const sessionMiddleware = session({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     // secure: process.env.NODE_ENV === "production",
     secure: false,
-    httpOnly: false,
+    httpOnly: true,
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   },
   genid: () => nanoid(21),
