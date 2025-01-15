ce2135fc3d32c15356fb217fb641742b95cf10ca
Huncho <hunchodotdev@gmail.com>
2025-01-15 09:35:52 +0000

    make secure

--- src/common/middleware/session.ts
+++ src/common/middleware/session.ts
@@ -57,9 +57,10 @@ const sessionMiddleware = session({
   proxy: undefined,
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    // secure: process.env.NODE_ENV === "production",
-    secure: false,
+    secure: process.env.NODE_ENV === "production",
+    // secure: false,
     httpOnly: true,
+    // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     domain: process.env.NODE_ENV === "production" ? process.env.COOKIE_DOMAIN : "localhost",
   },
